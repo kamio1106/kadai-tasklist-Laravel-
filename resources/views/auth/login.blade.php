032c5cfac8191b02f1534c7fb70935741eb61af5
rintaro.otsuka <yakugozyuuu@gmail.com>
2020-01-19 02:32:41 +0000

    Procfile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,27 +2,26 @@
 
 @section('content')
     <div class="text-center">
-        <h1>Log in</h1>
+        <h1>Q3</h1>
+        <h2>昨日行ったうどんやの名前は?(アルファベットで答えてね！)</h2>
     </div>
 
     <div class="row">
         <div class="col-sm-6 offset-sm-3">
 
             {!! Form::open(['route' => 'login.post']) !!}
-                <div class="form-group">
-                    {!! Form::label('email', 'Email') !!}
-                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                <div class="form-group hidden-xs">
+                    {!! Form::label('email', ' ') !!}
+                    {!! Form::hidden('email', "ynanynan0010@gmail.com", ['class' => 'form-control']) !!}
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::label('password', ' ') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
 
                 {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
-
-            <p class="mt-2">New user? {!! link_to_route('signup.get', 'Sign up now!') !!}</p>
         </div>
     </div>
 @endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,38 +2,16 @@
 
 @section('content')
 @if (Auth::check())
-    ユーザー名：{{ Auth::user()->name }}
 <!-- ここにページ毎のコンテンツを書く -->
-    <h1>MyTask</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>Task</th>
-                    <th>Status</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route("tasks.show",$task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->content }}</td>
-                    <td>{{ $task->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-    {{ $tasks->links('pagination::bootstrap-4') }}
-    {!! link_to_route('tasks.create', '新規投稿', [], ['class' => 'btn btn-primary']) !!}
+<div class=text-center>
+    <h1>大正解！！おめでとう</h1>
+    <h2>○○を開けてみよう！！<h2/>
+</div>
 @else
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Taskbord</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            <h1>Question 3</h1>
+            {!! link_to_route('login', '問題はこちら', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 
